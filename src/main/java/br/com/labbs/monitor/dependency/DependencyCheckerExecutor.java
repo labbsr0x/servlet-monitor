ba4fafb6166f6574f94cab8f1210e596a35ec71d
Rogerio A. Silva <rogerio_alexandre_dasilva@hotmail.com>
2022-12-27 00:00:40 -0300

    Add factory for DependencyCheckerExecutor for use in Server Container
    WebSphere Traditional or Liberty Profile

--- src/main/java/br/com/labbs/monitor/dependency/DependencyCheckerExecutor.java
+++ src/main/java/br/com/labbs/monitor/dependency/DependencyCheckerExecutor.java
@@ -8,23 +8,13 @@ import java.util.TimerTask;
  *
  * @see DependencyChecker
  */
-public class DependencyCheckerExecutor {
-    private static final long START_DELAY_MILLIS = 10000L;
-
-    private Timer timer;
-
-    public DependencyCheckerExecutor() {
-        timer = new Timer("monitor-metrics-dependency-checker");
-    }
+public interface DependencyCheckerExecutor {
 
     /**
      * Terminates the executor timer, discarding any currently scheduled tasks.
      * Removes all cancelled tasks from the executor timer's task queue.
      */
-    public void cancelTasks() {
-        timer.cancel();
-        timer.purge();
-    }
+    public void cancelTasks();
 
     /**
      * Schedules the specified task for repeated <i>fixed-rate execution period</i>.
@@ -32,7 +22,5 @@ public class DependencyCheckerExecutor {
      * @param task   task to be executed
      * @param period time in milliseconds between successive task executions.
      */
-    public void schedule(final TimerTask task, final long period) {
-        timer.scheduleAtFixedRate(task, START_DELAY_MILLIS, period);
-    }
+    public void schedule(final TimerTask task, final long period);
 }
